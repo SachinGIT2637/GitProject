1d2c7c84a30f712eebd773557fa122ff66316cce
SachinGIT2637 <sachin.gaikwadj22@gmail.com>
2025-04-02 20:27:18 +0530

    added code in sample3

--- src/main/java/Test1/Sample3.java
+++ src/main/java/Test1/Sample3.java
@@ -3,6 +3,11 @@ package Test1;
 public class Sample3 {
 
 	public static void main(String[] args) {
-		System.out.println("Test Code");
+		System.out.println("Test Code 1");
+		System.out.println("Test Code 2");
+		System.out.println("Test Code 3");
+		System.out.println("Test Code 4");
+		System.out.println("Test Code 5");
+		System.out.println("Test Code 6");
 	}
 }
