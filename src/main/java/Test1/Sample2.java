59a02e04800a9baccce71a799037de30d7371c75
SachinGIT2637 <sachin.gaikwadj22@gmail.com>
2025-04-02 17:48:01 +0530

    added new sample3 and removed whitespace

--- src/main/java/Test1/Sample2.java
+++ src/main/java/Test1/Sample2.java
@@ -5,6 +5,6 @@ public class Sample2 {
 	public static void main(String[] args) {
 		System.out.println("This is first code");
 		System.out.println("Develop branch code");
-                System.out.println("Code added by Git user x to develop branch");
+        System.out.println("Code added by Git user x to develop branch");
 	}
 }
